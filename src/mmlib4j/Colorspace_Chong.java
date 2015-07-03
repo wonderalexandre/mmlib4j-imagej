7b7a4e4ab33de5567cb5839309e477124b37e86a
wonderalexandre <wonderalexandre@192.168.0.29>
2015-07-03 01:06:49 -0300

    Color space plugin Paper: H.-Y. Chong, S. J. Gortler and T. Zickler, "A perception-based color space for illumination-invariant image processing," in SIGGRAPH, 2008, pp. 1-7.

---
+++ src/mmlib4j/Colorspace_Chong.java
@@ -0,0 +1,32 @@
+package mmlib4j;
+
+
+import ij.ImagePlus;
+import ij.gui.GenericDialog;
+import ij.plugin.filter.PlugInFilter;
+import ij.process.ColorProcessor;
+import ij.process.ImageProcessor;
+import mmlib4j.imagej.utils.ImageJAdapter;
+import mmlib4j.imagej.utils.ImageUtils;
+import mmlib4j.utils.ImageBuilder;
+
+/**
+ * MMLib4J - Mathematical Morphology Library for Java 
+ * @author Wonder Alexandre Luz Alves
+ *
+ * Graphic User Interface by ImageJ
+ */
+public class Colorspace_Chong implements PlugInFilter{
+	int levelOld;
+	int levelNew;
+	
+	@Override
+	public int setup(String arg, ImagePlus imp) {
+		return PlugInFilter.DOES_RGB;
+	}
+
+	@Override
+	public void run(ImageProcessor img) {
+		new ImagePlus("colorspace", ImageJAdapter.toByteProcessor( ImageBuilder.getConvertColorSpaceRGBtoChong( ImageJAdapter.toColorImage((ColorProcessor) img) ) )).show();
+	}
+}
